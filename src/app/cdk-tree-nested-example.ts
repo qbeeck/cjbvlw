1b01358ff9d8bded36913dcc81f93b19e94b4bda
qbeeck <64665123+qbeeck@users.noreply.github.com>
2023-05-29 12:44:39 +0300

    1 file modified

--- src/app/cdk-tree-nested-example.ts
+++ src/app/cdk-tree-nested-example.ts
@@ -2,31 +2,6 @@ import { ArrayDataSource } from '@angular/cdk/collections';
 import { Component } from '@angular/core';
 import { NestedTreeControl } from '@angular/cdk/tree';
 
-// interface FoodNode {
-//   name: string;
-//   children?: FoodNode[];
-// }
-
-// const TREE_DATA: FoodNode[] = [
-//   {
-//     name: 'Fruit',
-//     children: [{name: 'Apple'}, {name: 'Banana'}, {name: 'Fruit loops'}],
-//   },
-//   {
-//     name: 'Vegetables',
-//     children: [
-//       {
-//         name: 'Green',
-//         children: [{name: 'Broccoli'}, {name: 'Brussels sprouts'}],
-//       },
-//       {
-//         name: 'Orange',
-//         children: [{name: 'Pumpkins'}, {name: 'Carrots'}],
-//       },
-//     ],
-//   },
-// ];
-
 interface Category {
   id: number;
   name: string;
@@ -190,6 +165,4 @@ export class CdkTreeNestedExample {
   dataSource = new ArrayDataSource([categroyMapper(catalog)]);
   hasChild = (_: number, node: any) =>
     !!node.children && node.children.length > 0;
-
-  constructor() {}
 }
